853f2b2eda6ae2f971ca278251f488327572a8c7
Andriy <andrijbuluk@gmail.com>
2026-06-08 18:01:48 +0200

    Aded: class Meta for serializers

--- author/serializers.py
+++ author/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 
+from author.models import Author
+
 
 class AuthorSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(max_length=64)
     last_name = serializers.CharField(max_length=64)
-    pseudonym = serializers.CharField(max_length=64, allow_null=True, required=False)
+    pseudonym = serializers.CharField(
+        max_length=64,
+        allow_null=True,
+        required=False,
+    )
     age = serializers.IntegerField()
     retired = serializers.BooleanField()
+
+    class Meta:
+        model = Author
+        fields = (
+            "first_name",
+            "last_name",
+            "pseudonym",
+            "age",
+            "retired",
+        )
