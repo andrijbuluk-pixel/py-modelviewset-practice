6527582a839ecb8c5364f046e26f525b5afe4106
Andriy <andrijbuluk@gmail.com>
2026-06-08 18:01:24 +0200

    Fix: urls router

--- author/urls.py
+++ author/urls.py
@@ -1,27 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
 from author.views import AuthorViewSet
 
 
-author_list = AuthorViewSet.as_view(
-    {
-        "get": "list",
-        "post": "create",
-    }
-)
-
-author_detail = AuthorViewSet.as_view(
-    {
-        "get": "retrieve",
-        "put": "update",
-        "patch": "partial_update",
-        "delete": "destroy",
-    }
-)
+router = routers.DefaultRouter()
+router.register("author", AuthorViewSet, basename="manage")
 
 urlpatterns = [
-    path("author/", author_list, name="author_list"),
-    path("author/<int:pk>/", author_detail, name="author_detail"),
+    path("", include(router.urls)),
 ]
-
 
 app_name = "author"
