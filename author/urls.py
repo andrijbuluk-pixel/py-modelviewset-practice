82f6534fa7abb7669183586cf4a83d13e3fbfbd2
Andriy <andrijbuluk@gmail.com>
2026-06-08 17:50:52 +0200

    Aded: urls for CRUD

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,27 @@
-# Create your urls here
+from django.urls import path
+from author.views import AuthorViewSet
 
-urlpatterns = []
+
+author_list = AuthorViewSet.as_view(
+    {
+        "get": "list",
+        "post": "create",
+    }
+)
+
+author_detail = AuthorViewSet.as_view(
+    {
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy",
+    }
+)
+
+urlpatterns = [
+    path("author/", author_list, name="author_list"),
+    path("author/<int:pk>/", author_detail, name="author_detail"),
+]
+
+
+app_name = "author"
